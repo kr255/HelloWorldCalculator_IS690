e284297a1c006b0f5dd9651069da26f0ddb922c0
kamran raza <kr255@njit.edu>
2021-06-02 21:09:35 -0400

    first commit, calculator works as shown in the tutorial

--- HelloWorldCalculator/Program.cs
+++ HelloWorldCalculator/Program.cs
@@ -6,13 +6,57 @@ namespace HelloWorldCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            int a, b = 0;
-
+            //int a and b to store 2 numbers
+            int i, j = 0;
+            
+            //Welcome Dialog
             Console.WriteLine("Welcome To Hello World Calculator " + args[0] + " \n");
-            Console.WriteLine("Type a number, and then press Enter");
+            
+            //prompt for the first number
+            Console.WriteLine("Type the first number, and then press Enter");
+            i = Convert.ToInt32(Console.ReadLine());
+            
+            //prompt for the second number
+            Console.WriteLine("Type the second number, and then press Enter");
+            j = Convert.ToInt32(Console.ReadLine());
 
-            a = Convert.ToInt32(Console.ReadLine());
+
+
+            //options to choose from
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\tw - Multiply");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+
+            if (!(j.Equals(0))) {
+                Console.WriteLine("\td - Divide");
+            }
+            Console.Write("What will be your option? ");
+
+            switch (Console.ReadLine())
+            {
+                case "w":
+                    Console.WriteLine($"Your result: {i} * {j} = " + (i * j));
+                    break;
+                case "a":
+                    Console.WriteLine($"Your result: {i} + {j} = " + (i + j));
+                    break;
+                case "s":
+                    Console.WriteLine($"Your result: {i} - {j} = " + (i - j));
+                    break;
+                case "d":
+                    if (!(j.Equals(0)))
+                    {
+                        Console.WriteLine($"Your result: {i} / {j} = " + (i / j));
+                    }
+                    else { Console.WriteLine($"Tired to divide by { j }, the second number cant be { j }"); }
+                    break;
+
+            }
+            //close the application
+            Console.Write("Press any key to close the Calculator console app...");
+            Console.ReadKey();
+
         }
     }
 }
