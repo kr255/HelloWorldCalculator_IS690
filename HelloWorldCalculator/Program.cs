715e903945f024fdba9723194fdc49ac9ea88868
kamran raza <kamran raza@DESKTOP-KP90K2L>
2021-06-02 20:54:25 -0400

    Add project files.

---
+++ HelloWorldCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorldCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a, b = 0;
+
+            Console.WriteLine("Welcome To Hello World Calculator " + args[0] + " \n");
+            Console.WriteLine("Type a number, and then press Enter");
+
+            a = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
